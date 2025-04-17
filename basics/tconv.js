3f94ff263b8db4b4291644b3d40c8466d274939f
Damein098 <143063788+Damein098@users.noreply.github.com>
2025-04-17 16:05:00 +0000

    type conb

--- basics/tconv.js
+++ basics/tconv.js
@@ -1,9 +1,9 @@
 let score = undefined
-console.log(typeof(score));
+//console.log(typeof(score));
 
 let valueinno = Number(score)
-console.log(typeof valueinno)
-console.log(valueinno)
+//console.log(typeof valueinno)
+///console.log(valueinno)
 //datatypes have capital names 
 // "33" == 33
 // "33abc" == not a number /nan
@@ -11,7 +11,7 @@ console.log(valueinno)
 
 let isloggedin = ""
 let booleanloggedin = Boolean(isloggedin)
-console.log(booleanloggedin)
+//console.log(booleanloggedin)
 
 // 1==true 
 // "" == false
@@ -20,4 +20,43 @@ console.log(booleanloggedin)
 let someno = 33
 
 let stringno = String(someno)
-console.log(typeof stringno);
+//console.log(typeof stringno);
+
+
+
+
+
+
+
+
+
+
+
+
+////operations
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+let str1 = "hello"
+let str2 = " damien"
+
+let str3 = str1+ str2
+//console.log(str3)
+
+
+//console.log(1 + "2")
+//console.log(1+2+"2")
+//console.log( (3+4) *5 %3);
+//console.log(+true);
+//console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 =2 + 2 
+
+let gamecounter = 100
+++gamecounter;// prefix increment the increment operator increments and returns the value after incrementing.
+console.log(gamecounter)
+
+//postfix op ncrements and returns value before incrementing
