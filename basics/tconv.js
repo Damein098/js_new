5de56f9be9585dfe47dcedddd07a4913b1608341
Damein098 <143063788+Damein098@users.noreply.github.com>
2025-04-17 15:46:42 +0000

    new file:   basics/tconv.js

---
+++ basics/tconv.js
@@ -0,0 +1,23 @@
+let score = undefined
+console.log(typeof(score));
+
+let valueinno = Number(score)
+console.log(typeof valueinno)
+console.log(valueinno)
+//datatypes have capital names 
+// "33" == 33
+// "33abc" == not a number /nan
+// true = 1,false = 0
+
+let isloggedin = ""
+let booleanloggedin = Boolean(isloggedin)
+console.log(booleanloggedin)
+
+// 1==true 
+// "" == false
+// "hitesh" == true
+
+let someno = 33
+
+let stringno = String(someno)
+console.log(typeof stringno);
