86c6551924325d5a04c84dcb6ee551dbec39e70c
Damein098 <143063788+Damein098@users.noreply.github.com>
2025-05-17 15:40:16 +0000

    modified:   basics/04co.js

---
+++ basics/04co.js
@@ -0,0 +1,9 @@
+//console.log(2>1);
+console.log("2" > 1);
+console.log(null > 0);
+console.log(null >=0);  //js convert null to zero so null>=0 is truu
+console.log(undefined == 0);// false always
+
+//=== strict check checks the datatypes too
+console.log("2" === 2);
+
