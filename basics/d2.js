d5d5c1380295f8aa84f74e7d8d91b6f30c0dfc92
Damein098 <143063788+Damein098@users.noreply.github.com>
2025-05-20 15:48:53 +0000

    new file:   basics/d2.js

---
+++ basics/d2.js
@@ -0,0 +1,49 @@
+//primitive data types 
+// 7types  string number boolean null undefined symbol  bigint
+
+
+ const score = 100
+ const scorevalue = 100.3
+
+ const isloggedin = false 
+ const outsidetemp = null
+ let useremail; //let useremail = undefined
+ const id = Symbol('123')
+ const anotherid = Symbol('123')
+
+ console .log(id == anotherid)
+ const bigint = 12213213131311313131n
+
+
+
+
+//reference types (non primitive)
+
+// array objects functions 
+
+const heroes = ["ironman","hulk","thor"]
+let myobj=
+{
+    name:"damien",
+    age:22,
+}
+
+const myfunc = function(){
+    console.log("ehllo world")
+}
+
+console.log(typeof myfunc)
+
+
+//Primitive datatypes:
+String - string
+Number - number
+Boolean - Boolean
+Null - object(****)
+Undefined - undefined
+BigInt - bigint
+
+Non-primitive datatypes:
+Array - object
+Function - function(object)
+Object - object
