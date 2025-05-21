cc10b5da9802f370f8340384accf8ddbbe6b387a
Damein098 <143063788+Damein098@users.noreply.github.com>
2025-05-21 15:27:33 +0000

    new file:   basics/num.js

---
+++ basics/num.js
@@ -0,0 +1,36 @@
+const score = 400
+
+const balance = new Number(100)
+//console.log(balance);
+//console.log(score);
+
+///console.log(balance.toString());
+//console.log(balance.toFixed(2));
+
+const othernumber = 23.1291
+
+//console.log(othernumber.toPrecision(2))//works on 2 first numbers 
+
+const hundreds = 10000000
+
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+///////////////maths/////////////////////////////////////////////////////////////////
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// // console.log(Math.min(4,4,2,2,1,22,7,6));
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max -min +1)) + min)
