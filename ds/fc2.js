aaa71ca0c9a8bd874dac7b47b549fe7ea2be0bf3
Damien <143063788+Damein098@users.noreply.github.com>
2025-05-27 15:06:52 +0000

    new file:   ds/fc2.js

---
+++ ds/fc2.js
@@ -0,0 +1,23 @@
+///scopes 
+
+let a  = 300 // global scope
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("inner  ",a);
+    
+}//scopes when it comes with if else function
+//block scope
+
+// console.log(a);
+// console.log(b);
+console.log(a);
+
+
+
+
+
+
+
