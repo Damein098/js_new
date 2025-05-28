4ce7c629e9b9c0d04eaa51720bb84d6296697d6a
Damien <143063788+Damein098@users.noreply.github.com>
2025-05-28 15:15:04 +0000

    modified:   ds/fc2.js

--- ds/fc2.js
+++ ds/fc2.js
@@ -6,18 +6,52 @@ if(true){
     let a = 10
     const b = 20
     var c = 30
-    console.log("inner  ",a);
+    // console.log("inner  ",a);
     
 }//scopes when it comes with if else function
 //block scope
 
 // console.log(a);
 // console.log(b);
-console.log(a);
+// console.log(a);
+
+function one(){
+    const username = "damien"
+
+    function two()
+{
+    const website = "yt"
+    console.log(username);
+    
+ }//console.log(website);
+two()
+
+}
+
+// one()//two can access values from one
 
 
 
+if(true){
+    const username = "damien"
+    if(username === "damien"){
+        const website = "  yt"
+        //console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
 
+///+++++++++++++++++++++++++++++++++++++++++++++++ concept
+console.log(addone(5))
+function addone(num) {
+    return num +1
+}
 
-
+addtwo(5)//hoisting error because this variable holds a function 
+const addtwo = function(num){
+    return num +2
+}
 
