09f2d97a306572e5fe96cb1776d77e73ced71e56
Damien <143063788+Damein098@users.noreply.github.com>
2025-05-29 14:35:54 +0000

    modified:   ds/iife.js

--- ds/iife.js
+++ ds/iife.js
@@ -12,4 +12,23 @@
 })('damien') //function definition ()//function call
 
 
+//how code is executed in javascript
+
+
+//code -->> global execution context === (stored in )this
+
+//function execution context 
+//eval execution context
+
+//code runs in 2 phases memory creation phase(memory allocation) and execution phase
+
+//1.global execution ==this
+//2.memory phase val1=undefined
+//3.execution phase val1=10  function addnum====new variable environment+execution thread
+//4.again memory phase and execution phase for new variable environment
+//it deletes itself once it completes its work new variable environment
+//call stack acts like a stack and uses LIFO rule
+
+
+
 
