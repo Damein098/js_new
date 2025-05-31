5e3af0221dcfb0d86c862a16c691eda4dac23680
Damien <143063788+Damein098@users.noreply.github.com>
2025-05-31 17:59:34 +0000

    new file:   ds/if.js

---
+++ ds/if.js
@@ -0,0 +1,92 @@
+///if
+
+// const isloggedin = true
+// const temp = 30
+// if(temp < 50){
+//     console.log("yes");
+    
+// } else
+// console.log("boasdosaosd");
+
+
+// const score = 200
+
+// if(score >100){
+//     const power = "fly"
+//     console.log(`user power ${power}`);
+// }
+
+const balance = 1000
+
+// if(balance >500 ) console.log("test ");
+
+// if (balance < 500 ) {
+//     console.log("less than");
+    
+// }else if (balance < 750){
+//     console.log("less than 750");
+    
+// }else if (balance < 1200){
+//     console.log("less than 1200");
+// }
+
+
+const userloggedin = true
+const debitcard = true
+//and 
+// if(userloggedin && debitcard){
+//     console.log("wllow to buy");
+    
+// }
+//or
+// if(userloggedin || debitcard){
+//     console.log("wllow to buy");
+    
+// }
+
+
+////////switch
+
+const month = 3 //if const ="mar"
+
+switch (month) {
+    case 1://case "jan"
+        console.log("jan");
+        
+        break;
+    case 2://const "feb"
+        console.log("feb");
+        
+        break;
+    case 3 :
+        console.log("mar");
+        
+        break;
+    case 4:
+        console.log("apr");//hold to copy and shift+alt+down arrow key
+        
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
